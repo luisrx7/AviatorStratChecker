db217325c58b175e37683a21d2dcf1a1c8c5d3c8
Luís Freitas <luisrx7_locax@hotmail.com>
2023-08-19 15:26:11 +0100

    add AntiMartingaleStrat

--- checker.py
+++ checker.py
@@ -1,27 +1,52 @@
-from strats.custom_strats import MartingaleStrat
+from strats.custom_strats import MartingaleStrat, AntiMartingaleStrat
 
 from stratchecker.stratchecker import Strat_checker
 
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(message)s', level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S')
+
 
 #set logger format 
 
 
 
 def main():
+    slices = []
+    
     strat = MartingaleStrat("Martingale",
                             start_balance=25,
                             base_bet=0.1,
                             max_bet=1,
                             multiplier=2,
-                            max_bets=5 )
+                            max_bets=1000 )
     print(strat.describe())
+    
+    
+    strat2 = AntiMartingaleStrat("AntiMartingale",
+                            start_balance=25,
+                            base_bet=0.1,
+                            max_bet=1,
+                            multiplier=2,
+                            max_bets=1000 )
+    
 
-    strat_checker = Strat_checker(strat, "results1.txt")
+    
+    strat_checker = Strat_checker(strat, "results.txt")
     strat_checker.read_results()
-    strat_checker.run()
+    strat_checker.run(count=5,random_slice=True)
+    slices = strat_checker.get_slices()
+    
+    strat_checker2 = Strat_checker(strat2, "results.txt")
+    strat_checker2.read_results()
+    strat_checker2.set_slices(slices)
+    strat_checker2.run(count=5,random_slice=False)
+     
+    
+    strat_checker.report()
+    
+    strat_checker2.report()
+    
     print(strat.balance)
 
 
--- strats/custom_strats.py
+++ strats/custom_strats.py
@@ -4,8 +4,8 @@ from strats.strats import Strat
 
 class MartingaleStrat(Strat):
 
-    def __init__(self,description,start_balance, base_bet, max_bet, multiplier ,max_bets=1000):
-        super().__init__(description,start_balance, base_bet, max_bet, multiplier ,max_bets=1000)
+    def __init__(self,description,start_balance, base_bet, max_bet, multiplier ,max_bets):
+        super().__init__(description,start_balance, base_bet, max_bet, multiplier ,max_bets)
         
 
     def on_win(self):
@@ -19,3 +19,30 @@ class MartingaleStrat(Strat):
         self.bet = self.bet * 2
 
 
+class AntiMartingaleStrat(Strat):
+    '''
+    Reverse Martingale
+    One compelling alternative is the Reverse Martingale, also known as 
+    the Anti-Martingale. As opposed to doubling your bet amount after a
+    loss at online casinos, the Reverse Martingale instructs you to 
+    double your bet amount after a win. When you lose, you go back to 
+    the start and bet 1 unit again.
+
+    This system is designed to capitalize on winning streaks. However, 
+    all of your previous profits can be wiped out with a single loss, 
+    so this system is best used in short bursts.
+    '''
+    
+    def __init__(self,description,start_balance, base_bet, max_bet, multiplier ,max_bets):
+        super().__init__(description,start_balance, base_bet, max_bet, multiplier ,max_bets)
+    
+    def on_win(self):
+        super().on_win()
+
+        self.bet = self.bet * 2
+
+    def on_lose(self):
+        super().on_lose()
+        
+        self.bet = self.base_bet
+        
